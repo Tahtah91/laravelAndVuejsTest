a494c60d1e0d77e0deac6d28ee04b1aa90f42b06
Tahtah91 <93201208+Tahtah91@users.noreply.github.com>
2024-01-17 19:15:28 +0300

    modification welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,6 +18,6 @@
     <body class="antialiased">
         <div id="app"></div>
 
-        <script src="{{ mix('js/app.js') }}"></script>
+        <script src="{{ mix('../js/app.js') }}"></script>
     </body>
 </html>
