bc8d1e821faf5bd59c3ed19d19a87908e1f0fa3b
Tahtah91 <93201208+Tahtah91@users.noreply.github.com>
2024-01-17 18:07:05 +0300

    simple vue component for login form

---
+++ resources/js/components/App.vue
@@ -0,0 +1,11 @@
+<template>
+    <div id="app">
+        <header class="title">
+            <h1>Login</h1>
+        </header>
+        <main>
+            <router-view></router-view>
+        </main>
+    </div>
+</template>
+
---
+++ resources/js/components/Login.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <h2>Login</h2>
+        <form @submit.prevent="login">
+        <div class="form-group">
+            <label for="username">Username:</label>
+            <input
+                type="text"
+                id="username"
+                v-model="username"
+                class="form-control"
+                required
+            />
+        </div>
+        <div class="form-group">
+            <label for="password">Password:</label>
+            <input
+                type="password"
+                id="password"
+                v-model="password"
+                class="form-control"
+                required
+            />
+        </div>
+        <button type="submit" class="btn btn-primary">Login</button>
+    </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                username: '',
+                password: ''
+            };
+        },
+        methods: {
+            login() {
+                // Authentication logic
+                console.log('Logging in with:', this.username, this.password);
+            }
+        }
+    };
+</script>
+
